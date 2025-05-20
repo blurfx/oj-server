10149e31ee8ad845e95917c9818ef5e4ce0fe042
changhui lee <iam@xo.dev>
2025-05-20 20:16:08 +0900

    implement problem api

--- internal/handler/problems.go
+++ internal/handler/problems.go
@@ -45,3 +45,44 @@ func V1GetProblems(req *V1GetProblemsRequest, c echo.Context) Response {
 		Data: problems,
 	}
 }
+
+type V1GetProblemRequest struct {
+	ID uint `json:"id"`
+}
+
+type Problem struct {
+	ID          uint   `db:"id" json:"id"`
+	Title       string `db:"title" json:"title"`
+	Description string `db:"description" json:"description"`
+	Spoiler     string `db:"spoiler" json:"spoiler"`
+	TimeLimit   uint   `db:"time_limit" json:"time_limit"`
+	MemoryLimit uint   `db:"memory_limit" json:"memory_limit"`
+}
+
+func V1GetProblem(_ *struct{}, c echo.Context) Response {
+	repo := dao.GetRepo()
+	id := c.Param("id")
+	rows, err := repo.Reader().Query("SELECT id, title, description, spoiler, time_limit, memory_limit FROM problem WHERE id = $1", id)
+
+	if err != nil {
+		panic(err)
+	}
+
+	problems := make([]Problem, 0)
+	for rows.Next() {
+		var problem Problem
+		rows.ScanStruct(&problem)
+		problems = append(problems, problem)
+	}
+
+	if len(problems) == 0 {
+		return Response{
+			Code: http.StatusNotFound,
+		}
+	}
+
+	return Response{
+		Code: http.StatusOK,
+		Data: problems[0],
+	}
+}
--- internal/handler/route.go
+++ internal/handler/route.go
@@ -7,4 +7,5 @@ func InitV1Handler(e *echo.Echo) {
 	e.GET("v1/auth/login", BaseHandler(LoginRequest{}, V1Login))
 	e.GET("v1/auth/logout", BaseHandler(LogoutRequest{}, V1Logout))
 	e.GET("v1/problems", BaseHandler(V1GetProblemsRequest{}, V1GetProblems))
+	e.GET("v1/problems/:id", BaseHandler(struct{}{}, V1GetProblem))
 }
