f4c2b4b33739e66dca6342169a4bf23f642112bc
blurfx <iam@xo.dev>
2025-06-02 01:01:45 +0900

    add eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,12 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import stylistic from '@stylistic/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +17,58 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+      '@stylistic': stylistic,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      'no-use-before-define': 'off',
+      'max-len': 'off',
+      '@stylistic/keyword-spacing': 'error',
+      '@stylistic/quotes': [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+        },
+      ],
+      'no-console': [
+        'error',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+      '@stylistic/space-before-blocks': 'error',
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@stylistic/semi': ['off'],
+      '@stylistic/type-annotation-spacing': [
+        'error',
+        {
+          before: false,
+          after: true,
+          overrides: {
+            colon: {
+              before: false,
+              after: true,
+            },
+            arrow: {
+              before: true,
+              after: true,
+            },
+          },
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
